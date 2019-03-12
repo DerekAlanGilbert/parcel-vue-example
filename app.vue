e4eb0d0e70201393349cc534e0dafe9e2f263bfe
Derek Gilbert <derek@thedevelopmentphase.com>
2019-03-12 09:51:31 -0600

    initial commit

---
+++ app.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div>
+    Hello {{ bundler }}
+  </div>
+</template>
+
+<script lang="js">
+import Vue from "vue";
+export default {
+  data() {
+    return {
+      bundler: "Parcel"
+    };
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  color: green;
+}
+</style>
